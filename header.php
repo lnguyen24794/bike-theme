bca18062e7cc9dc18069135f730103d9e760f3f4
lnguyen24794 <lnguyen24794@gmail.com>
2025-04-20 14:24:10 +0700

    Update header.php

--- header.php
+++ header.php
@@ -175,11 +175,15 @@
                     <div class="col-lg-7 px-5 text-start">
                         <div class="h-100 d-inline-flex align-items-center py-2 me-4">
                             <i class="fa fa-envelope text-primary me-2"></i>
-                            <p class="mb-0"><?php echo esc_html(bike_theme_get_option('email', 'info@beebikehub.com')); ?></p>
+                            <p class="mb-0"><?php echo esc_html(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?></p>
                         </div>
                         <div class="h-100 d-inline-flex align-items-center py-2">
                             <i class="fa fa-phone-alt text-primary me-2"></i>
-                            <p class="mb-0"><?php echo esc_html(bike_theme_get_option('phone', '+84985455727')); ?></p>
+                            <p class="mb-0"><?php echo esc_html(bike_theme_get_option('contact_phone', '+84985455727')); ?></p>
+                        </div>
+                        <div class="h-100 d-inline-flex align-items-center py-2">
+                            <i class="fa fa-whatsapp text-primary me-2"></i>
+                            <p class="mb-0"><?php echo esc_html(bike_theme_get_option('whatsapp_phone', '+84985455727')); ?></p>
                         </div>
                     </div>
                     <div class="col-lg-5 px-5 text-end">
