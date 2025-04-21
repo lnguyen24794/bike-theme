f332cde64accc84078ab184de63d438d37fd42db
lnguyen24794 <lnguyen24794@gmail.com>
2025-04-21 22:59:40 +0700

    Update single-bike_tour.php

--- single-bike_tour.php
+++ single-bike_tour.php
@@ -208,7 +208,7 @@ $active_tab = isset($_GET['tab']) ? sanitize_text_field($_GET['tab']) : 'overvie
                 <div class="col-lg-8 mt-0">
                     <!-- Tour Tabs Start -->
                     <div class="mb-5">
-                        <ul class="nav nav-tabs mb-4 d-flex justify-content-center tour-tabs pt-4" id="tourTab" role="tablist">
+                        <ul class="nav nav-tabs d-flex justify-content-center tour-tabs py-4 shadow" id="tourTab" role="tablist">
                             <li class="nav-item" role="presentation">
                                 <button class="nav-link <?php echo $active_tab === 'overview' ? 'active' : ''; ?>" id="overview-tab" data-bs-toggle="tab" data-bs-target="#overview" type="button" role="tab" aria-controls="overview" aria-selected="<?php echo $active_tab === 'overview' ? 'true' : 'false'; ?>">
                                     <?php esc_html_e('Overview', 'bike-theme'); ?>
