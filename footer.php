12dfbd6a1c336284fb897ddd33a36f2238777bb0
lnguyen24794 <lnguyen24794@gmail.com>
2025-04-24 22:48:25 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,10 +17,10 @@
                     <?php if (is_active_sidebar('footer-1')) : ?>
                         <?php dynamic_sidebar('footer-1'); ?>
                     <?php else : ?>
-                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i><a href="<?php echo esc_url(bike_theme_get_option('address_link', '#')); ?>" target="_blank"><?php echo esc_html(bike_theme_get_option('contact_address', '123 Street, New York, USA')); ?></a></p>
-                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a href="tel:<?php echo esc_attr(bike_theme_get_option('contact_phone', '+849854557270')); ?>"><?php echo esc_html(bike_theme_get_option('contact_phone', '+849854557270')); ?></a></p>
-                        <p class="mb-2"><i class="fab fa-whatsapp me-3"></i><a href="https://api.whatsapp.com/send/?phone=<?php echo esc_attr(bike_theme_get_option('contact_phone', '84985455727')); ?>&text&type=phone_number&app_absent=0"><?php echo esc_html(bike_theme_get_option('whatsapp_phone', '+849854557270')); ?></a></p>
-                        <p class="mb-2"><i class="fa fa-envelope me-3"></i><a href="mailto:<?php echo esc_attr(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?>"><?php echo esc_html(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?></a></p>
+                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i><a class="text-light" href="<?php echo esc_url(bike_theme_get_option('address_link', '#')); ?>" target="_blank"><?php echo esc_html(bike_theme_get_option('contact_address', '123 Street, New York, USA')); ?></a></p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a class="text-light" href="tel:<?php echo esc_attr(bike_theme_get_option('contact_phone', '+849854557270')); ?>"><?php echo esc_html(bike_theme_get_option('contact_phone', '+849854557270')); ?></a></p>
+                        <p class="mb-2"><i class="fab fa-whatsapp me-3"></i><a class="text-light" href="https://api.whatsapp.com/send/?phone=<?php echo esc_attr(bike_theme_get_option('contact_phone', '84985455727')); ?>&text&type=phone_number&app_absent=0"><?php echo esc_html(bike_theme_get_option('whatsapp_phone', '+849854557270')); ?></a></p>
+                        <p class="mb-2"><i class="fa fa-envelope me-3"></i><a class="text-light" href="mailto:<?php echo esc_attr(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?>"><?php echo esc_html(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?></a></p>
                     <?php endif; ?>
                 </div>
                 <div class="col-lg-3 col-md-6">
@@ -50,7 +50,7 @@
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4"><?php esc_html_e('Fanpage', 'bike-theme'); ?></h4>
                     <div>
-                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBeeBike.BikeTours.and.BikeRentals%2F&tabs&width=340&height=70&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=2174410252844536" width="340" height="70" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                    <iframe loading="lazy" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBeeBike.BikeTours.and.BikeRentals%2F&tabs&width=340&height=70&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=2174410252844536" width="340" height="70" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                     </div>
                 </div>
             </div>
@@ -96,7 +96,7 @@
             }
         });
     }
-jQuery(document).ready(function($) {
+    jQuery(document).ready(function($) {
     // Initialize lightbox for gallery images
     $('.gallery-lightbox').on('click', function(e) {
         e.preventDefault();
