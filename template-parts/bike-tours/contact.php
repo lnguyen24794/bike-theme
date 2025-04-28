82435bc988921848f43fa0e1033b3f3c0883f2c5
lnguyen24794 <lnguyen24794@gmail.com>
2025-04-28 09:40:36 +0700

    Update contact.php

--- template-parts/bike-tours/contact.php
+++ template-parts/bike-tours/contact.php
@@ -7,10 +7,10 @@
             <p><?php esc_html_e('For questions about this tour or to make a reservation, please contact us:', 'bike-theme'); ?></p>
             
             <div class="mt-4">
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i><a class="text-light" href="<?php echo esc_url(bike_theme_get_option('address_link', '#')); ?>" target="_blank"><?php echo esc_html(bike_theme_get_option('contact_address', '123 Street, New York, USA')); ?></a></p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a class="text-light" href="tel:<?php echo esc_attr(bike_theme_get_option('contact_phone', '+849854557270')); ?>"><?php echo esc_html(bike_theme_get_option('contact_phone', '+849854557270')); ?></a></p>
-                <p class="mb-2"><i class="fab fa-whatsapp me-3"></i><a class="text-light" href="https://api.whatsapp.com/send/?phone=<?php echo esc_attr(bike_theme_get_option('contact_phone', '84985455727')); ?>&text&type=phone_number&app_absent=0"><?php echo esc_html(bike_theme_get_option('whatsapp_phone', '+849854557270')); ?></a></p>
-                <p class="mb-2"><i class="fa fa-envelope me-3"></i><a class="text-light" href="mailto:<?php echo esc_attr(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?>"><?php echo esc_html(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?></a></p>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i><a  href="<?php echo esc_url(bike_theme_get_option('address_link', '#')); ?>" target="_blank"><?php echo esc_html(bike_theme_get_option('contact_address', '123 Street, New York, USA')); ?></a></p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a  href="tel:<?php echo esc_attr(bike_theme_get_option('contact_phone', '+849854557270')); ?>"><?php echo esc_html(bike_theme_get_option('contact_phone', '+849854557270')); ?></a></p>
+                <p class="mb-2"><i class="fab fa-whatsapp me-3"></i><a  href="https://api.whatsapp.com/send/?phone=<?php echo esc_attr(bike_theme_get_option('contact_phone', '84985455727')); ?>&text&type=phone_number&app_absent=0"><?php echo esc_html(bike_theme_get_option('whatsapp_phone', '+849854557270')); ?></a></p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i><a  href="mailto:<?php echo esc_attr(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?>"><?php echo esc_html(bike_theme_get_option('contact_email', 'info@beebikehub.com')); ?></a></p>
             </div>
         </div>
     <?php endif; ?>
