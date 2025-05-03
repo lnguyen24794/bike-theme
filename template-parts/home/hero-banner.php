31e1e8b003f62fd89801267cf84c646a0f76a7fd
lnguyen24794 <lnguyen24794@gmail.com>
2025-05-03 08:34:10 +0700

    Update hero-banner.php

--- template-parts/home/hero-banner.php
+++ template-parts/home/hero-banner.php
@@ -43,7 +43,7 @@
                                 <img  src="<?php echo $title ?>"  class="relative mb-2 animated slideInDown" style=" position: static; max-width: 550px; max-height: 130px;" />
                             <?php endif; ?>
                             <?php if (!empty($subtitle)) : ?>
-                                <p style="margin-bottom: 30px;" class="text-shadow text-white text-size-medium mb-3 animated slideInDown"><?php echo esc_html($subtitle); ?></p>
+                                <p style="margin-bottom: 30px;" class="text-shadow text-white text-size-x-medium mb-3 animated slideInDown"><?php echo esc_html($subtitle); ?></p>
                             <?php endif; ?>
                             <div class="w-lg-50 mx-auto">
                                 <?php if (!empty($btn1_text)) : ?>
