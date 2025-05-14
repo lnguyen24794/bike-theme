61a61a9262e258f889ec11802c7b6736f66310ca
lnguyen24794 <lnguyen24794@gmail.com>
2025-05-14 21:44:10 +0700

    remove price table

--- template-parts/bike-tours/price-and-services.php
+++ template-parts/bike-tours/price-and-services.php
@@ -8,37 +8,6 @@
             <div class="price-info wp-editor-content mb-2">
                 <?php echo wp_kses_post(get_post_meta(get_the_ID(), '_tour_price_info', true)); ?>
             </div>
-            <?php if (get_post_meta(get_the_ID(), '_tour_flexible_pricing_enabled', true) === '1') :
-                $pricing_data = get_post_meta(get_the_ID(), '_tour_flexible_pricing', true);
-                if (!empty($pricing_data) && is_array($pricing_data)) :
-                    usort($pricing_data, function ($a, $b) {
-                        return $a['participants'] - $b['participants'];
-                    });
-                    ?>
-                <div class="table-responsive">
-                    <table class="table table-bordered">
-                        <thead class="table-light">
-                            <tr>
-                                <th><?php esc_html_e('Number of Participants', 'bike-theme'); ?></th>
-                                <th><?php esc_html_e('Price per Person', 'bike-theme'); ?></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($pricing_data as $price_item) : ?>
-                            <tr>
-                                <td><?php printf(esc_html(_n('%d person', '%d people', $price_item['participants'], 'bike-theme')), $price_item['participants']); ?></td>
-                                <td><?php echo bike_theme_format_price($price_item['price']); ?></td>
-                            </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-            <?php else : ?>
-                <p><?php echo bike_theme_format_price(bike_theme_get_tour_price(get_the_ID())); ?> <?php esc_html_e('per person', 'bike-theme'); ?></p>
-            <?php endif; ?>
-            <?php else : ?>
-                <p><?php echo bike_theme_format_price(bike_theme_get_tour_price(get_the_ID())); ?> <?php esc_html_e('per person', 'bike-theme'); ?></p>
-            <?php endif; ?>
         </div>
 
         <div class="row">
