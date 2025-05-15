a738a4a9ae116346b28711621c7cc8f9c10ccbd7
lnguyen24794 <lnguyen24794@gmail.com>
2025-05-15 22:25:09 +0700

    Revert "update"
    This reverts commit 6c2349724e5e5622c6357cfaa5207f81f6682809.

--- template-parts/bike-tours/tour-slider.php
+++ template-parts/bike-tours/tour-slider.php
@@ -35,7 +35,7 @@
                 autoplayTimeout: 3000,
                 autoplayHoverPause: true,
                 stagePadding: 50,
-                autoWidth:false,
+                autoWidth:true,
                 center: true,
                 navText: [
                     "<i class='fa fa-chevron-left'></i>",
