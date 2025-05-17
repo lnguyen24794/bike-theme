549b53109ef4d694cfe5f90eb6dd2967eeaaaef6
lnguyen24794 <lnguyen24794@gmail.com>
2025-05-17 11:50:12 +0700

    Update header.php

--- header.php
+++ header.php
@@ -70,7 +70,7 @@ wp_enqueue_style('home-header', get_template_directory_uri() . '/assets/css/home
                         $secondary_logo = bike_theme_get_option('secondary_logo');
                     ?>
                     
-                    <div class="container-fluid" style="max-width: 400px;">
+                    <div class="container-fluid justify-content-center align-items-center" style="max-width: 400px;">
                         <div class="row">
                             <div class="col-lg-3 pl-2" style="padding-left: 20px; padding-right: 0px;">
                                 <img src="<?php echo esc_url($logo[0]); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" class="img-fluid">
