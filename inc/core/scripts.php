ae4f56fb72873ae903687ffba523ab175642d382
lnguyen24794 <lnguyen24794@gmail.com>
2025-05-18 19:30:33 +0700

    change swiper

--- inc/core/scripts.php
+++ inc/core/scripts.php
@@ -28,7 +28,7 @@ function bike_theme_scripts()
 
     // Libraries CSS
     wp_enqueue_style('bike-theme-animate', get_template_directory_uri() . '/lib/animate/animate.min.css', array(), BIKE_THEME_VERSION);
-    wp_enqueue_style('bike-theme-owl-carousel', get_template_directory_uri() . '/lib/owlcarousel/assets/owl.carousel.min.css', array(), BIKE_THEME_VERSION);
+    wp_enqueue_style('bike-theme-swiper', get_template_directory_uri() . '/assets/css/swiper-bundle.min.css', array(), BIKE_THEME_VERSION);
     wp_enqueue_style('bike-theme-tempusdominus', get_template_directory_uri() . '/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css', array(), BIKE_THEME_VERSION);
 
     // Bootstrap
@@ -56,7 +56,7 @@ function bike_theme_scripts()
     wp_enqueue_script('bike-theme-easing', get_template_directory_uri() . '/lib/easing/easing.min.js', array('jquery'), BIKE_THEME_VERSION, true);
     wp_enqueue_script('bike-theme-waypoints', get_template_directory_uri() . '/lib/waypoints/waypoints.min.js', array('jquery'), BIKE_THEME_VERSION, true);
     wp_enqueue_script('bike-theme-counterup', get_template_directory_uri() . '/lib/counterup/counterup.min.js', array('jquery'), BIKE_THEME_VERSION, true);
-    wp_enqueue_script('bike-theme-owl-carousel', get_template_directory_uri() . '/lib/owlcarousel/owl.carousel.min.js', array('jquery'), BIKE_THEME_VERSION, true);
+    wp_enqueue_script('bike-theme-swiper', get_template_directory_uri() . '/assets/js/swiper-bundle.min.js', array('jquery'), BIKE_THEME_VERSION, true);
     wp_enqueue_script('bike-theme-moment', get_template_directory_uri() . '/lib/tempusdominus/js/moment.min.js', array('jquery'), BIKE_THEME_VERSION, true);
     wp_enqueue_script('bike-theme-moment-timezone', get_template_directory_uri() . '/lib/tempusdominus/js/moment-timezone.min.js', array('jquery', 'bike-theme-moment'), BIKE_THEME_VERSION, true);
     wp_enqueue_script('bike-theme-tempusdominus', get_template_directory_uri() . '/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js', array('jquery', 'bike-theme-moment', 'bike-theme-moment-timezone'), BIKE_THEME_VERSION, true);
--- template-parts/home/tour-gallery.php
+++ template-parts/home/tour-gallery.php
@@ -4,56 +4,50 @@ $tour_gallery = bike_theme_get_option('tour_gallery', array());
 if (count($tour_gallery) > 0) :
     ?>
 
-<div class="container-fluid mt-5 py-5 bg-gray">
-    <!-- Grid Gallery Layout -->
-    <div class="gallery-slide owl-carousel">
-        <?php foreach($tour_gallery as $index => $gallery_item):
-            $image_url = !empty($gallery_item['image_url']) ? $gallery_item['image_url'] : 'large';
-            if (!empty($image_url)) :
-                ?>
-            <div class="gallery-item" style="height: min(100vw, 300px);">
-                <a href="<?php echo esc_url($image_url); ?>" class="gallery-lightbox">
-                    <?php echo wp_get_attachment_image($gallery_item['image_id'], 'full', false, array(
-                                'class' => 'img-fluid rounded box-shadow',
-                                'style' => 'width: 100%; height: 100%; object-fit: cover;'
-                            )); ?>
-                </a>
-             </div>
-        <?php
-            endif;
-        endforeach; ?>
-    </div>
+<div class="container-fluid mt-5 py-5 bg-gray swiper-container">
+   <!-- Slider main container -->
+    <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <?php foreach($tour_gallery as $index => $gallery_item):
+                    $image_url = !empty($gallery_item['image_url']) ? $gallery_item['image_url'] : 'large';
+                    if (!empty($image_url)) :
+                        ?>
+                    <div class="swiper-slide">
+                         <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($gallery_item['title']); ?>">
+                    </div>
+                <?php endif; endforeach; ?>
+        </div>
 
-    <script>
-        jQuery(document).ready(function($){
-            $('.gallery-slide').owlCarousel({
-                loop: true,
-                margin: 50,
-                nav: true,
-                dots: true,
-                autoplay: false,
-                autoplayTimeout: 3000,
-                autoplayHoverPause: true,
-                stagePadding: 50,
-                autoWidth:true,
-                center: true,
-                navText: [
-                    "<i class='fa fa-chevron-left'></i>",
-                    "<i class='fa fa-chevron-right'></i>"
-                ],
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    768:{
-                        items:2
-                    },
-                    992:{
-                        items:3
-                    }
-                }
-            });
-        });
-    </script>
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+    </div>
 </div>
+
+
+
+<script>
+    jQuery(document).ready(function($){
+        var slidesPerView = 3;
+        if (window.innerWidth < 768) {
+            slidesPerView = 1;
+        }
+        const swiper = new Swiper(".swiper", {
+            effect: "coverflow",
+            rewind: true,
+            slidesPerView: slidesPerView,
+            freeMode: true,
+            loop: true,
+            spaceBetween: 30,
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    });
+</script>
 <?php endif; ?>
