0b9237b44222646c49d6c4ab90956846a639eeea
lnguyen24794 <lnguyen24794@gmail.com>
2025-06-15 14:24:32 +0700

    Update itinerary.php

--- template-parts/bike-tours/itinerary.php
+++ template-parts/bike-tours/itinerary.php
@@ -31,9 +31,9 @@
                                     <div class="col-lg-8 day-description content-formatted">
                                         <div class="timeline">
                                             <?php
-                                if (!empty($day['timeline_items'])) {
-                                    foreach ($day['timeline_items'] as $key => $item) {
-                                        ?>
+                                            if (!empty($day['timeline_items'])) {
+                                                foreach ($day['timeline_items'] as $key => $item) {
+                                                    ?>
                                                     <div class="timeline-item right">
                                                         <div class="content shadow">
                                                             <h2><?php echo esc_html($item['title']); ?></h2>
@@ -41,22 +41,26 @@
                                                         </div>
                                                         <?php if($key !== count($day['timeline_items']) - 1) : ?>
                                                         <div class="timeline-icon">
-                                                            <i class="fas fa-<?php echo esc_attr($item['icon']); ?>"></i>
+                                                            <i class="fas fa-<?php if($key ===  count($day['timeline_items']) - 1) {
+                                                                echo 'map-marker-alt';
+                                                            } else {
+                                                                echo esc_attr($item['icon']);
+                                                            } ?>"></i>
                                                         </div>
                                                         <?php endif; ?>
                                                     </div>
                                                     <?php
-                                    }
-                                } else {
-                                    // Fallback content if no timeline items
-                                    ?>
-                                                <div class="timeline-item right">
-                                                    <div class="content shadow">
-                                                        <div class="wp-editor-content"><?php echo wp_kses_post($day['description']); ?></div>
-                                                    </div>
-                                                </div>
-                                                <?php
-                                }
+                                                }
+                                            } else {
+                                                // Fallback content if no timeline items
+                                                ?>
+                                                            <div class="timeline-item right">
+                                                                <div class="content shadow">
+                                                                    <div class="wp-editor-content"><?php echo wp_kses_post($day['description']); ?></div>
+                                                                </div>
+                                                            </div>
+                                                            <?php
+                                            }
                         ?>
                                         </div>
                                     </div>
@@ -85,7 +89,7 @@
                                                             <h5><?php esc_html_e('Meals Provided', 'bike-theme'); ?></h5>
                                                             <div class="meals-included wp-editor-content">
                                                                 <?php
-                                            $meals_included = array();
+                        $meals_included = array();
                                                     $meal_icons = array(
                                                         'breakfast' => '<i class="fas fa-coffee"></i>',
                                                         'lunch' => '<i class="fas fa-hamburger"></i>',
