50754a7c4b83cdf06d200a6c6677e5c80bf113b3
lnguyen24794 <lnguyen24794@gmail.com>
2025-08-29 13:51:29 +0700

    add Favicon và Icons

--- header.php
+++ header.php
@@ -14,7 +14,13 @@
     <meta content="<?php echo get_bloginfo('description'); ?>" name="description">
     <meta content="<?php echo esc_attr(bike_theme_get_option('meta_keywords', 'bikes, cycling, bicycle')); ?>" name="keywords">
     
-    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <!-- Favicon và Icons -->
+    <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.jpg">
+    <link rel="icon" type="image/jpg" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.jpg">
+    <link rel="icon" type="image/jpg" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.jpg">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.jpg">
+    <link rel="icon" type="image/jpg" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.jpg">
+    <link rel="icon" type="image/jpg" sizes="512x512" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon_512.jpg">
     
 <?php wp_head();
 wp_enqueue_style('home-header', get_template_directory_uri() . '/assets/css/home.css', array(), BIKE_THEME_VERSION); ?>
@@ -56,7 +62,7 @@ wp_enqueue_style('home-header', get_template_directory_uri() . '/assets/css/home
                 'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
                 'walker'          => new WP_Bootstrap_Navwalker()
             ));
-            ?>
+?>
         </div>
     </div>
 
@@ -64,11 +70,11 @@ wp_enqueue_style('home-header', get_template_directory_uri() . '/assets/css/home
         <div class="row gx-0">
             <div class="col-lg-3 bg-primary d-none d-lg-block">
                 <a href="<?php echo esc_url(home_url('/')); ?>" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
-                    <?php 
-                        $custom_logo_id = get_theme_mod('custom_logo');
-                        $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
-                        $secondary_logo = bike_theme_get_option('secondary_logo');
-                    ?>
+                    <?php
+            $custom_logo_id = get_theme_mod('custom_logo');
+$logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+$secondary_logo = bike_theme_get_option('secondary_logo');
+?>
                     
                     <div class="container-fluid" style="max-width: 400px;">
                         <div class="row align-items-center justify-content-center">
@@ -145,7 +151,7 @@ wp_enqueue_style('home-header', get_template_directory_uri() . '/assets/css/home
                                 'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                                 'walker'            => new WP_Bootstrap_Navwalker()
                             ));
-                        ?>
+?>
                     </div>
                 </nav>
             </div>
@@ -184,9 +190,4 @@ wp_enqueue_style('home-header', get_template_directory_uri() . '/assets/css/home
                 }
             }
         });
-    </script>
-</div>
-
-<?php wp_footer(); ?>
-</body>
-</html> 
+    </script> 
